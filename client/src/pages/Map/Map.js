9a35768df454ffa97a610d449644b46808e8220b
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2022-10-12 18:45:24 +0300

    add geojsons

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -1,39 +1,93 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+
+import geojson from "../../assets/Detail_Level_01_Regions_EPSG32629.json"
+import geojson2 from "../../assets/Detail_Level_01_Regions_WGS84.json";
+import geojson3 from "../../assets/Detail_Level_02_Communes_EPSG32629.json";
+import geojson4 from "../../assets/Detail_Level_02_Communes_WGS84.json";
+import geojson5 from "../../assets/Detail_Level_03_FarmLeaders_Farms_Fields_WGS84.json";
 
 import "./styles.scss";
 
 const MapBox = () => {
 
-    React.useEffect(() => {
-        const L = require("leaflet");
-    
-        delete L.Icon.Default.prototype._getIconUrl;
-    
-        L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-        iconUrl: require("leaflet/dist/images/marker-icon.png"),
-        shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-        });
-    }, []);
+    // const [coordinates, setCoordinates] = useState([]);
 
+    // useEffect(()=>{
+    //     setCoordinates(JSON.parse(geojson.features).map(row=> [row[1], row[0]]))
+    // },[])
 
-    useEffect(()=>{
-        console.log('mapRendered')
-    },[])
+    const setColor = ({ properties }) => {
+        return { 
+            "color": "#ff7800",
+            "weight": 2,
+            "opacity": 0.65
+        };
+      };
+
+      const setColor2 = ({ properties }) => {
+        return { 
+            "color": "blue",
+            "weight": 2,
+            "opacity": 0.65
+        };
+      };
+
+      const setColor3 = ({ properties }) => {
+        return { 
+            "color": "red",
+            "weight": 2,
+            "opacity": 0.65
+        };
+      };
+
+      const setColor4 = ({ properties }) => {
+        return { 
+            "color": "green",
+            "weight": 2,
+            "opacity": 0.65
+        };
+      };
+
+      const setColor5 = ({ properties }) => {
+        return { 
+            "color": "yellow",
+            "weight": 2,
+            "opacity": 0.65
+        };
+      };
+
+    // React.useEffect(() => {
+    //     const L = require("leaflet");
+    
+    //     delete L.Icon.Default.prototype._getIconUrl;
+    
+    //     L.Icon.Default.mergeOptions({
+    //     iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    //     iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    //     shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    //     });
+    // }, []);
+
     return(
         <div id="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                {/* <Marker position={[51.505, -0.09]}>
                     <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                </Marker>
+                </Marker> */}
+                {/* <Polygon positions={coordinates} /> */}
+                    <GeoJSON key='geojson' data={geojson} style={setColor} />
+                    <GeoJSON key='geojson2' data={geojson2} style={setColor2} />     
+                    <GeoJSON key='geojson3' data={geojson3} style={setColor3} />   
+                    <GeoJSON key='geojson4' data={geojson4} style={setColor4} />   
+                    <GeoJSON key='geojson5' data={geojson5} style={setColor5} />   
             </MapContainer>
         </div>
     )
